d6021f8cde06d5cf2ceec03251aed474ef362af6
Carlos A <cayala.w@gmail.com>
2024-09-19 00:51:01 -0500

    implement app to register, eliminate costs

--- inmersion-dev-aula2-Proyecto-Base/assets/js/script.js
+++ inmersion-dev-aula2-Proyecto-Base/assets/js/script.js
@@ -1,15 +1,18 @@
-const buttonAddCost = document.getElementById("botonFormulario");
 const costsNames = [];
 const costsValues = [];
+const totalCost = document.getElementById("totalGastos");
+let totalCostSummatory = 0;
+const costsListSpace = document.getElementById("listaDeGastos");
 
 function clickBoton() {
   let costValue = document.getElementById("valorGasto");
   let costName = document.getElementById("nombreGasto");
-  console.log("costValue:", costValue.value, " costName:", costName.value);
   costValueOver150(costValue.value, costName.value);
   addCost(costName.value, costValue.value);
-  console.log("costsNames:", costsNames);
-  console.log("costsValues:", costsValues);
+  totalCostSummatory += Number(costValue.value);
+  cleanInputs();
+  updateCostsListSpace();
+  totalCost.innerText = totalCostSummatory.toFixed(2);
 }
 
 function costValueOver150(costValue, costName) {
@@ -20,4 +23,25 @@ function costValueOver150(costValue, costName) {
 function addCost(costName, costValue) {
   costsNames.push(costName);
   costsValues.push(Number(costValue));
+}
+
+function updateCostsListSpace() {
+  let list = "";
+  for (let i = 0; i < costsNames.length; i++) {
+    list += `<li>${costsNames[i]} - USD ${costsValues[i]}<button onclick="deleteItem(${i})">Eliminar</button></li>`;
+  }
+  costsListSpace.innerHTML = list;
+}
+
+function cleanInputs() {
+  document.getElementById("valorGasto").value = "";
+  document.getElementById("nombreGasto").value = "";
+}
+
+function deleteItem(index) {
+  totalCostSummatory -= costsValues[index];
+  totalCost.innerText = totalCostSummatory.toFixed(2);
+  costsNames.splice(index, 1);
+  costsValues.splice(index, 1);
+  updateCostsListSpace();
 }
