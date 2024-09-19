da819dbab2c8c24451532c87bc0175ca406e4cb2
Carlos A <cayala.w@gmail.com>
2024-09-19 01:58:36 -0500

    add cost description field

--- inmersion-dev-aula2-Proyecto-Base/assets/js/script.js
+++ inmersion-dev-aula2-Proyecto-Base/assets/js/script.js
@@ -1,5 +1,6 @@
 const costsNames = [];
 const costsValues = [];
+const costsDescriptions = [];
 let totalCostSummatory = 0;
 const totalCost = document.getElementById("totalGastos");
 const costsListSpace = document.getElementById("listaDeGastos");
@@ -7,8 +8,9 @@ const costsListSpace = document.getElementById("listaDeGastos");
 function clickBoton() {
   let costValue = document.getElementById("valorGasto");
   let costName = document.getElementById("nombreGasto");
+  let costDescription = document.getElementById("descripcionGasto");
   costValueOver150(costValue.value, costName.value);
-  addCost(costName.value, costValue.value);
+  addCost(costName.value, costValue.value, costDescription.value);
   totalCostSummatory += Number(costValue.value);
   cleanInputs();
   updateCostsListSpace();
@@ -21,8 +23,9 @@ function costValueOver150(costValue, costName) {
   }
 }
 
-function addCost(costName, costValue) {
+function addCost(costName, costValue, costDescription) {
   costsNames.push(costName);
+  costsDescriptions.push(costDescription);
   costsValues.push(Number(costValue));
 }
 
@@ -31,9 +34,9 @@ function updateCostsListSpace() {
   let item = "";
   for (let i = 0; i < costsNames.length; i++) {
     if (costsValues[i] > 150) {
-      item = `<li style="color: red">${costsNames[i]} - USD ${costsValues[i]}</span><button onclick="deleteItem(${i})">Eliminar</button></li>`;
+      item = `<li class="highlight"><article><p>${costsNames[i]} - USD ${costsValues[i]}</p><p>${costsDescriptions[i]}</p></article><button onclick="deleteItem(${i})">Eliminar</button></li>`;
     } else {
-      item = `<li>${costsNames[i]} - USD ${costsValues[i]}<button onclick="deleteItem(${i})">Eliminar</button></li>`;
+      item = `<li><article><p>${costsNames[i]} - USD ${costsValues[i]}</p><p>${costsDescriptions[i]}</p></article><button onclick="deleteItem(${i})">Eliminar</button></li>`;
     }
     list += item;
   }
@@ -42,6 +45,7 @@ function updateCostsListSpace() {
 
 function cleanInputs() {
   document.getElementById("valorGasto").value = "";
+  document.getElementById("descripcionGasto").value = "";
   document.getElementById("nombreGasto").value = "";
 }
 
@@ -49,6 +53,7 @@ function deleteItem(index) {
   totalCostSummatory -= costsValues[index];
   totalCost.innerText = totalCostSummatory.toFixed(2);
   costsNames.splice(index, 1);
+  costsDescriptions.splice(index, 1);
   costsValues.splice(index, 1);
   updateCostsListSpace();
 }
