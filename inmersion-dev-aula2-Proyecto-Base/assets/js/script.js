44a7a7fa9d028aa9963cbedcbcb9c953ae18f408
Carlos A <cayala.w@gmail.com>
2024-09-19 22:05:23 -0500

    modify items - styles for expensive items - code to modify items

--- inmersion-dev-aula2-Proyecto-Base/assets/js/script.js
+++ inmersion-dev-aula2-Proyecto-Base/assets/js/script.js
@@ -1,26 +1,19 @@
 const costsNames = [];
 const costsValues = [];
 const costsDescriptions = [];
-let totalCostSummatory = 0;
 const totalCost = document.getElementById("totalGastos");
 const costsListSpace = document.getElementById("listaDeGastos");
 
+// add items to costsList
 function clickBoton() {
   let costValue = document.getElementById("valorGasto");
   let costName = document.getElementById("nombreGasto");
   let costDescription = document.getElementById("descripcionGasto");
   costValueOver150(costValue.value, costName.value);
   addCost(costName.value, costValue.value, costDescription.value);
-  totalCostSummatory += Number(costValue.value);
-  cleanInputs();
-  updateCostsListSpace();
-  totalCost.innerText = totalCostSummatory.toFixed(2);
-}
-
-function costValueOver150(costValue, costName) {
-  if (Number(costValue) > 150) {
-    alert(`El costo de ${costName} es mayor a USD 150`);
-  }
+  cleanForm();
+  showCostsList();
+  totalCost.innerText = calculateTotalCost().toFixed(2);
 }
 
 function addCost(costName, costValue, costDescription) {
@@ -28,32 +21,76 @@ function addCost(costName, costValue, costDescription) {
   costsDescriptions.push(costDescription);
   costsValues.push(Number(costValue));
 }
-
-function updateCostsListSpace() {
+// helper
+function costValueOver150(costValue, costName) {
+  if (Number(costValue) > 150) {
+    alert(`El costo de ${costName} es mayor a USD 150`);
+  }
+}
+//helper
+function calculateTotalCost() {
+  return costsValues.reduce((acc, cost) => acc + cost, 0);
+}
+// showCostsList
+function showCostsList() {
   let list = "";
   let item = "";
   for (let i = 0; i < costsNames.length; i++) {
+    const buttons = `<section><button onclick="modifyItem(${i})">Modificar</button><button onclick="deleteItem(${i})">Eliminar</button></section>`;
     if (costsValues[i] > 150) {
-      item = `<li class="highlight"><article><p>${costsNames[i]} - USD ${costsValues[i]}</p><p>${costsDescriptions[i]}</p></article><button onclick="deleteItem(${i})">Eliminar</button></li>`;
+      item = `<li class="highlight"><article><p>${costsNames[i]} - USD ${costsValues[i]}</p><p>${costsDescriptions[i]}</p></article>${buttons}</li>`;
     } else {
-      item = `<li><article><p>${costsNames[i]} - USD ${costsValues[i]}</p><p>${costsDescriptions[i]}</p></article><button onclick="deleteItem(${i})">Eliminar</button></li>`;
+      item = `<li><article><p>${costsNames[i]} - USD ${costsValues[i]}</p><p>${costsDescriptions[i]}</p></article>${buttons}</li>`;
     }
     list += item;
   }
   costsListSpace.innerHTML = list;
 }
 
-function cleanInputs() {
+function cleanForm() {
   document.getElementById("valorGasto").value = "";
   document.getElementById("descripcionGasto").value = "";
   document.getElementById("nombreGasto").value = "";
 }
 
 function deleteItem(index) {
-  totalCostSummatory -= costsValues[index];
-  totalCost.innerText = totalCostSummatory.toFixed(2);
   costsNames.splice(index, 1);
   costsDescriptions.splice(index, 1);
   costsValues.splice(index, 1);
-  updateCostsListSpace();
+  showCostsList();
+  totalCost.innerText = calculateTotalCost().toFixed(2);
+}
+
+function modifyItem(index) {
+  document.getElementById("nombreGasto").value = costsNames[index];
+  document.getElementById("descripcionGasto").value = costsDescriptions[index];
+  document.getElementById("valorGasto").value = costsValues[index];
+  changeButton("modify", index);
+}
+
+function modifyCost(index) {
+  costsNames[index] = document.getElementById("nombreGasto").value;
+  costsDescriptions[index] = document.getElementById("descripcionGasto").value;
+  costsValues[index] = Number(document.getElementById("valorGasto").value);
+  costValueOver150(costsValues[index], costsNames[index]);
+  cleanForm();
+  showCostsList();
+  totalCost.innerText = calculateTotalCost().toFixed(2);
+  changeButton("add");
+}
+
+function changeButton(type, index = null) {
+  const formButton = document.getElementById("botonFormulario");
+  console.log("formButton", formButton);
+  if (type === "modify") {
+    console.log("type:", type);
+    formButton.innerText = "Modificar Gasto";
+    formButton.setAttribute("onclick", `modifyCost(${index})`);
+  }
+
+  if (type === "add") {
+    console.log("type:", type);
+    formButton.innerText = "Agregar Gasto";
+    formButton.setAttribute("onclick", "clickBoton()");
+  }
 }
